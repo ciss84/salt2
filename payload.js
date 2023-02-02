bb66ef7153b45e717aeacb496eeaf6dd57069198
Mugiwara <37186888+ciss84@users.noreply.github.com>
2023-02-02 13:20:12 +0100

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,382 @@
+//GoldHEN
+function GoldHEN212() {
+    ShowMSG = "Loading GoldHEN 2.1.2!"
+    loadmsg()
+    PLfile = "GoldHEN212.bin"
+    loadBIN()
+}
+function GoldHEN224() {
+    ShowMSG = "Loading GoldHEN 2.2.4!"
+    loadmsg()
+    PLfile = "GoldHEN224.bin"
+    loadBIN()
+}function GoldHEN225b8() {
+    ShowMSG = "Loading GoldHEN 2.2.5b8!"
+    loadmsg()
+    PLfile = "GoldHEN225b8.bin"
+    loadBIN()
+}
+function GoldHEN23() {
+    ShowMSG = "Loading GoldHEN 2.3!"
+    loadmsg()
+    PLfile = "GoldHEN23.bin"
+    loadBIN()
+}
+// other
+function app2usb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "app2usb.bin"
+    loadBIN()
+}
+function appdumper() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "appdumper.bin"
+    loadBIN()
+}
+function dupdate() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "dupdate.bin"
+    loadBIN()
+}
+function eupdate() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "eupdate.bin"
+    loadBIN()
+}
+function ftp() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "ftp.bin"
+    loadBIN()
+}
+function dbbackup() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "dbbackup.bin"
+    loadBIN()
+}
+function dbrestore() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "dbrestore.bin"
+    loadBIN()
+}
+function pkgbackup() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "pkg_backup11.bin"
+    loadBIN()
+}
+function Mdumper() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "moduledumper.bin"
+    loadBIN()
+}
+function Kdumper() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "kerneldumper.bin"
+    loadBIN()
+}
+function PUART() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "puart.bin"
+    loadBIN()
+}
+function PCon() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "ps4parentalcontrols.bin"
+    loadBIN()
+}
+function rifRename() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "rifren.bin"
+    loadBIN()
+}
+function daslr() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "daslr.bin"
+    loadBIN()
+}
+function dtodex() {
+    PLfile = "dtodex.bin"
+    loadBIN()
+}
+function etodex() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "etodex.bin"
+    loadBIN()
+}
+function Exitidu() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = ".bin"
+    loadBIN()
+}
+function orbistool() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "exitidu.bin"
+    loadBIN()
+}
+function ps4debug() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "ps4debug.bin"
+    loadBIN()
+}
+function webrte() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "webrte.bin"
+    loadBIN()
+}
+function southbridge() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "whos_that_southbridge.bin"
+    loadBIN()
+}
+//linux
+function linux1gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader1gb.bin"
+    loadBIN()
+}
+function linux2gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader2gb.bin"
+    loadBIN()
+}
+function linux3gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader3gb.bin"
+    loadBIN()
+}
+function linux4gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader4gb.bin"
+    loadBIN()
+}
+function linux5gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader5gb.bin"
+    loadBIN()
+}
+//linuxn
+function linuxn1gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader_n1gb.bin"
+    loadBIN()
+}
+function linuxn2gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader_n2gb.bin"
+    loadBIN()
+}
+function linuxn3gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader_n3gb.bin"
+    loadBIN()
+}
+function linuxn4gb() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Linux/linuxloader_n4gb.bin"
+    loadBIN()
+}
+// GTAV Mods
+function L100() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/GTAVLamance100b.bin"
+    loadBIN()
+}
+function L138() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/gtavlamance138.bin"
+    loadBIN()
+}
+function A127() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/arabicguy127.bin"
+    loadBIN()
+}
+function A132() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/arabicguy132.bin"
+    loadBIN()
+}
+function A133() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/arabicguy133.bin"
+    loadBIN()
+}
+function B133() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/BeefQueefMod133.bin"
+    loadBIN()
+}
+function B134() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/BeefQueefMod134.bin"
+    loadBIN()
+}
+function B138() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/BeefQueefMod138.bin"
+    loadBIN()
+}
+function E138() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/expulsion138.bin"
+    loadBIN()
+}
+function W132() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/wildemodz132.bin"
+    loadBIN()
+}
+function W133() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/wildemodz133.bin"
+    loadBIN()
+}
+function W138() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/wildemodz138.bin"
+    loadBIN()
+}
+// RDR2 Mods
+function r100() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/OystersMenu100.bin"
+    loadBIN()
+}
+function r113() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/OystersMenu113.bin"
+    loadBIN()
+}
+function r119() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/OystersMenu119.bin"
+    loadBIN()
+}
+function r124() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/OystersMenu124.bin"
+    loadBIN()
+}
+function r129() {
+    ShowMSG = "Loading_Payload!"
+    loadmsg()
+    PLfile = "Gmods/OystersMenu129.bin"
+    loadBIN()
+}
+
+
+// Linux & Game Mods
+function linuxVram() {
+    var get = document.getElementById("linux").value
+    if (get == "1") {
+        linux1gb()
+    }else if (get == "2") {
+        linux2gb()
+    }else if (get == "3") {
+        linux3gb()
+    }else if (get == "4") {
+        linux4gb()
+    }else if (get == "5") {
+        linux5gb()
+    }
+}
+function linuxnVram() {
+    var get = document.getElementById("linuxn").value
+    if (get == "1") {
+        linuxn1gb()
+    }else if (get == "2") {
+        linuxn2gb()
+    }else if (get == "3") {
+        linuxn3gb()
+    }else if (get == "4") {
+        linuxn4gb()
+    }
+}
+function GTAVGmod() {
+    var get = document.getElementById("gtav").value
+    if (get == "L100") {
+        L100()
+    }else if (get == "L138") {
+        L138()
+    }else if (get == "A127") {
+        A127()
+    }else if (get == "A132") {
+        A132()
+    }else if (get == "A133") {
+        A133()
+    }else if (get == "B133") {
+        B133()
+    }else if (get == "B134") {
+        B134()
+    }else if (get == "B138") {
+        B138()
+    }else if (get == "E138") {
+        E138()
+    }else if (get == "W132") {
+        W132()
+    }else if (get == "W133") {
+        W133()
+    }else if (get == "W138") {
+        W138()
+    }
+}
+function rdr2Gmod() {
+    var get = document.getElementById("rdr2").value
+    if (get == "100") {
+        r100()
+    }else if (get == "113") {
+        r113()
+    }else if (get == "119") {
+        r119()
+    }else if (get == "124") {
+        r124()
+    }else if (get == "129") {
+        r129()
+    }
+}
+
+// Codded / developed by @bishalqx980
---
+++ status.js
@@ -0,0 +1,78 @@
+function s2() {
+    document.getElementById("status").innerHTML = "GoldHEN [PS4 HomeBrew Enabler] Run to play Fpkg Games!"
+}
+function s3() {
+    document.getElementById("status").innerHTML = "Copy/Move Internal HDD Games/Apps to External Drive!"
+}
+function s4() {
+    document.getElementById("status").innerHTML = "Dump Bluray Drive Game to HDD!"
+}
+function s5() {
+    document.getElementById("status").innerHTML = "Disable System Updates!"
+}
+function s6() {
+    document.getElementById("status").innerHTML = "Enable System Updates!"
+}
+function s7() {
+    document.getElementById("status").innerHTML = "Run a Full PS4 HDD access FTP Server!"
+}
+function s8() {
+    document.getElementById("status").innerHTML = "Database Backup!"
+}
+function s9() {
+    document.getElementById("status").innerHTML = "Database Restore!"
+}
+function s10() {
+    document.getElementById("status").innerHTML = "Dump PKG to External USB! (Modded Version of App2USB)"
+}
+function s11() {
+    document.getElementById("status").innerHTML = "Dump Module!"
+}
+function s12() {
+    document.getElementById("status").innerHTML = "Dump Kernal to External USB as BIN file!"
+}
+function s13() {
+    document.getElementById("status").innerHTML = "Permanent UART!"
+}
+function s14() {
+    document.getElementById("status").innerHTML = "Show Parental Control Password!"
+}
+function s15() {
+    document.getElementById("status").innerHTML = "RIF Renamer!"
+}
+function s16() {
+    document.getElementById("status").innerHTML = "Disable ASLR!"
+}
+function s17() {
+    document.getElementById("status").innerHTML = "Disable ToDex!"
+}
+function s18() {
+    document.getElementById("status").innerHTML = "Enable ToDex!"
+}
+function s19() {
+    document.getElementById("status").innerHTML = "Exit IDU mode!"
+}
+function s20() {
+    document.getElementById("status").innerHTML = "Orbis ToolBox!"
+}
+function s21() {
+    document.getElementById("status").innerHTML = "PS4Debug!"
+}
+function s22() {
+    document.getElementById("status").innerHTML = "WebRTE!"
+}
+function s23() {
+    document.getElementById("status").innerHTML = "Show The SouthBridge! (important for Linux user)"
+}
+function s24() {
+    document.getElementById("status").innerHTML = "Select VRam'Linux'"
+}
+function s25() {
+    document.getElementById("status").innerHTML = "Select VRam 'LinuxN'"
+}
+function s26() {
+    document.getElementById("status").innerHTML = "Select Game Version 'GTAV'"
+}
+function s27() {
+    document.getElementById("status").innerHTML = "Select Game Version 'RDR2'"
+}
